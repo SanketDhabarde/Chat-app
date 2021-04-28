d2bb99939cb30ee2026db636d57024d2b16dbd98
SanketDhabarde <sanketdhabarde13@gmail.com>
2021-04-28 12:14:58 +0530

    first commit

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,20 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <Avatar/>
+            </div>
+            <div className="sidebar__search">
+
+            </div>
+            <div className="sidebar__chats">
+
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDyYA_Je1DfGt09ChIZRP8uhbKgZthrIj4",
+    authDomain: "chat-app-4f37d.firebaseapp.com",
+    projectId: "chat-app-4f37d",
+    storageBucket: "chat-app-4f37d.appspot.com",
+    messagingSenderId: "587880578600",
+    appId: "1:587880578600:web:1bf4ef17cbb94d98367f1d",
+    measurementId: "G-G4QKMKE5WB"
+  };
