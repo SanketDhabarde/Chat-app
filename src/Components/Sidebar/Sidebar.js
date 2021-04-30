bd007722f08aa1325ac9b7665a149c0e7ac527a0
SanketDhabarde <sanketdhabarde13@gmail.com>
2021-04-30 19:51:25 +0530

    feat: add search feature

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -3,7 +3,7 @@ import { SearchOutlined } from '@material-ui/icons';
 import ChatIcon from '@material-ui/icons/Chat';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState, useContext, useRef } from 'react';
 import SidebarChat from '../SidebarChat/SidebarChat';
 import './Sidebar.css';
 import db from '../../firebase';
@@ -12,6 +12,10 @@ import { AuthContext} from '../../context/auth-context';
 function Sidebar() {
     const authContext = useContext(AuthContext);
     const [rooms, setRooms] = useState([]);
+    const [enteredFilter, setEnteredFilter] = useState('');
+    const [filteredRooms, setFilteredRooms] = useState([]);
+
+    const inputRef = useRef();
 
     useEffect(()=> {
        const unsubscribe= db.collection('rooms').onSnapshot(snapshot => {
@@ -25,6 +29,24 @@ function Sidebar() {
             unsubscribe();
         }
     }, []);
+
+    useEffect(() => {
+       const timer = setTimeout(() => {
+            if(enteredFilter === inputRef.current.value){
+                db.collection('rooms').where("name", "==", enteredFilter).onSnapshot(snapshot => {
+                    setFilteredRooms(snapshot.docs.map(doc => ({
+                        id:doc.id,
+                        data: doc.data()
+                    })))
+                });
+            }  
+        }, 1000);
+
+        return () => {
+            clearTimeout(timer);
+        }
+        
+    }, [enteredFilter, inputRef])
 
 
     return (
@@ -47,13 +69,21 @@ function Sidebar() {
             <div className="sidebar__search">
                 <div className="sidebar__searchContainer">
                     <SearchOutlined/>
-                    <input placeholder="search chat room" type="text"/>
+                    <input 
+                        ref={inputRef}
+                        placeholder="search chat room" 
+                        type="text" 
+                        value={enteredFilter} 
+                        onChange={event => setEnteredFilter(event.target.value)}/>
                 </div>  
             </div>
             <div className="sidebar__chats">
                 <SidebarChat addNew/>
-                {
+                { !enteredFilter ?
                     rooms.map(room => (
+                        <SidebarChat key={room.id} id={room.id} name={room.data.name}/>
+                    )) :
+                    filteredRooms.map(room => (
                         <SidebarChat key={room.id} id={room.id} name={room.data.name}/>
                     ))
                 }
--- src/Components/SidebarChat/SidebarChat.js
+++ src/Components/SidebarChat/SidebarChat.js
@@ -1,8 +1,9 @@
-import { Avatar } from '@material-ui/core';
+import { Avatar, IconButton } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import './SidebarChat.css';
 import db from '../../firebase';
 import { Link } from 'react-router-dom';
+import { Delete } from '@material-ui/icons';
 
 function SidebarChat({id, name, addNew}) {
     const [seed, setSeed] = useState('');
@@ -32,6 +33,7 @@ function SidebarChat({id, name, addNew}) {
         });
     }
 
+    
     return !addNew ? (
         <Link to={`/rooms/${id}`}>
             <div className="sidebarChat">
