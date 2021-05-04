8d43c18911b71820fbf989a11d2cc61705551a84
SanketDhabarde <sanketdhabarde13@gmail.com>
2021-05-04 16:18:52 +0530

    style: selected chat room

--- src/Components/SidebarChat/SidebarChat.js
+++ src/Components/SidebarChat/SidebarChat.js
@@ -2,7 +2,7 @@ import { Avatar } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import './SidebarChat.css';
 import db from '../../firebase';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Image } from '@material-ui/icons';
 
 function SidebarChat({id, name}) {
@@ -25,15 +25,15 @@ function SidebarChat({id, name}) {
     }, [id])
 
     return (
-        <Link to={`/rooms/${id}`}>
+        <NavLink to={`/rooms/${id}`} activeClassName="sidebarChat__active">
             <div className="sidebarChat">
                 <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
                 <div className="sidebarChat__info">
                     <h3>{name}</h3>
-                    <p>{messages[0]?.message ? messages[0].message.substring(0,10) + '...' : <Image/>}</p>
+                    <p>{messages[0]?.message ? messages[0].message.substring(0,20) + '...' : <Image/>}</p>
                 </div>
             </div>
-        </Link>
+        </NavLink>
         
     )
 }
