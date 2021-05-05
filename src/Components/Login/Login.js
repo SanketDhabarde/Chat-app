745bbc0491e9173dd6ae9eefad54a31edbfe4e20
SanketDhabarde <sanketdhabarde13@gmail.com>
2021-05-05 18:51:50 +0530

    style: add logo

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -17,10 +17,10 @@ function Login() {
                 <img
                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/225px-WhatsApp.svg.png" alt="whatsapp logo"/>
                  <div className="login__text">
-                     <h1>sign in to WhatsApp</h1>
+                     <h2>Sign in to WhatsApp-Clone</h2>
                  </div>
                 <Link to="/rooms">
-                    <Button onClick={signin}>Sign in with Google</Button>
+                    <Button onClick={signin}><img className="login__googleLogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/768px-Google_%22G%22_Logo.svg.png"/>Sign in with Google</Button>
                 </Link>
                  
             </div>
